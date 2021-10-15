a54eed27046306502b5b5b56515c2c534ced94c8
adgor <adgor000@gmail.com>
2021-10-16 01:19:45 +0200

    fixing pricing section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,38 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        matisse: {
+          DEFAULT: "#1F5AA6",
+          50: "#B9D2F2",
+          100: "#A3C4EE",
+          200: "#78A8E6",
+          300: "#4D8CDE",
+          400: "#2771D1",
+          500: "#1F5AA6",
+          600: "#17437B",
+          700: "#0F2B50",
+          800: "#071425",
+          900: "#000000",
+        },
+        zircon: {
+          50: "#ffffff",
+          100: "#fefeff",
+          200: "#fdfdff",
+          300: "#fbfcfe",
+          400: "#f8fafe",
+          500: "#f5f8fd",
+          600: "#dddfe4",
+          700: "#b8babe",
+          800: "#939598",
+          900: "#787a7c",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
