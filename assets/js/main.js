7b011e4594d21621e641d9a4154414fd8e94c060
adgor <adgor000@gmail.com>
2021-10-19 23:40:13 +0200

    fixing & addin in HP

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,3 +47,21 @@ const listHandler = (check) => {
     chevronup.classList.remove("hidden");
   }
 };
+
+let elements = document.querySelectorAll("[data-menu]");
+for (let i = 0; i < elements.length; i++) {
+  let main = elements[i];
+  main.addEventListener("click", function () {
+    let element = main.parentElement.parentElement;
+    let andicators = main.querySelectorAll("svg");
+    let child = element.querySelector("p");
+    child.classList.toggle("hidden");
+    if (child.classList.contains("hidden")) {
+      andicators[0].style.display = "block";
+      andicators[1].style.display = "none";
+    } else {
+      andicators[0].style.display = "none";
+      andicators[1].style.display = "block";
+    }
+  });
+}
